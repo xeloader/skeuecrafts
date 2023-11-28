f0ebec79bdb0f5f36f3933abf6f244ff2294d10e
Victor Ingman <viregistermail@gmail.com>
2023-11-28 16:27:03 +0100

    :lipstick: Hover action on button

--- src/components/ep133-ui/Button.tsx
+++ src/components/ep133-ui/Button.tsx
@@ -1,4 +1,4 @@
-import React, { StyleHTMLAttributes } from 'react'
+import React, { StyleHTMLAttributes, useState } from 'react'
 
 import ButtonTextureDark from '@/images/ep133/texture-noise-dark.png'
 
@@ -36,58 +36,50 @@ const buttonStyles: ButtonStyles = {
     texture: {
       backgroundImage: `url(${ButtonTextureDark})`,
       backgroundSize: '75%',
-      opacity: 0.1
+      opacity: 0.05
     }
   }
 }
 
-type HoleStyles = { [key in Environment]?: React.CSSProperties }
-
-const holeStyles: HoleStyles = {
-  [Environment.Dark]: {
-    boxShadow: '1px 1px 1px rgba(255,255,255,0.2)'
-  }
-}
-
-interface HoleProps {
-  children?: JSX.Element,
-  environment?: Environment
-}
-
-export function Hole ({
-  children,
-  environment = Environment.Dark
-}: HoleProps) {
-  return (
-    <div className='relative'>
-      <div 
-        style={holeStyles?.[environment]}
-        className='flex flex-col items-center justify-center p-[2px] bg-[#050404]'>
-        {children}
-      </div>
-    </div>
-  )
+interface SquareButtonState {
+  active: boolean,
+  hover: boolean
 }
 
 export function SquareButton ({
   color
 }: SquareButtonProps) {
+  const [state, setState] = useState<SquareButtonState>({
+    active: false,
+    hover: false
+  })
   return (
-    <Hole>
-      <div className='relative'>
-        <button className='z-10 relative w-24 h-24 flex flex-col items-center bg-[#171717] justify-center bg-gradient-to-br from-white/50 to-60% to-black/50 rounded-2 p-[0.1rem] shadow-[inset_2px_2px_2px_rgba(255,255,255,0.1)]'>
-          <div className='absolute -bottom-3 -right-2 w-24 h-24 bg-black blur-[0.5rem] z-0 rounded-2 opacity-80'></div>
-          <div className='relative h-full w-full overflow-hidden rounded-[calc(0.5rem-1px)]'>
-            <div 
-              className='absolute z-10 h-full w-full'
-              style={buttonStyles?.[color]?.texture}>
-            </div>
-            <div 
-              className='w-full h-full bg-[#1E1E1E]'>
-            </div>
-          </div>
-        </button>
+    <div 
+      className='relative' 
+      data-active={state}
+      onMouseDown={() => setState(state => ({ ...state, active: true }))}
+      onMouseUp={() => setState(state => ({ ...state, active: false }))}
+      onMouseOver={() => setState(state => ({ ...state, hover: true }))}
+      onMouseOut={() => setState(state => ({ ...state, hover: false }))}>
+      <div className={state.hover === true ? 'opacity-0' : 'opacity-100'}>
+        <div className='absolute left-0 bottom-0 w-3 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,0.3)]'/>
+        <div className='absolute right-0 bottom-0 w-1 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,0.3)]' />
+        <div className='absolute right-0 top-0 w-[1px] h-6 shadow-[1px_0px_1px_rgba(255,255,255,0.3)]' />
       </div>
-    </Hole>
+      <div className='flex flex-col items-center justify-center p-[2px] rounded-[1px] bg-[#050404]'>
+        <div className='relative'>
+          <button className='z-10 relative w-24 h-24 flex flex-col items-center bg-[#171717] justify-center bg-gradient-to-br from-white/50 to-60% to-black/50 rounded-2 p-[0.1rem] shadow-[inset_2px_2px_2px_rgba(255,255,255,0.1)]'>
+            <div className='absolute -bottom-3 -right-2 w-24 h-24 bg-black blur-[0.5rem] z-0 rounded-2 opacity-80'></div>
+            <div className='relative h-full w-full overflow-hidden rounded-[calc(0.5rem-1px)]'>
+              <div 
+                className='absolute z-10 h-full w-full'
+                style={buttonStyles?.[color]?.texture}>
+              </div>
+              <div className='w-full h-full bg-[#1E1E1E]' />
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
