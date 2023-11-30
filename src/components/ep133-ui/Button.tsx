8e19831a1b3e2c58142a08e132d9154b24a40e93
Victor Ingman <viregistermail@gmail.com>
2023-11-30 16:53:05 +0100

    :lipstick: Convert to tailwind and fix details of button

--- src/components/ep133-ui/Button.tsx
+++ src/components/ep133-ui/Button.tsx
@@ -1,6 +1,6 @@
 import React, { StyleHTMLAttributes, useState } from 'react'
 
-import ButtonTextureDark from '@/images/ep133/texture-noise-dark.png'
+const DarkNoiseTexture = require('@/images/ep133/texture-noise-dark.png')
 
 export enum Colors {
   LightGray = 'light-gray',
@@ -25,22 +25,6 @@ type ButtonStyles = {
   }
 }
 
-const buttonStyles: ButtonStyles = {
-  [Colors.Dark]: {
-    base: {
-      background: 'linear-gradient(135deg, rgba(255, 255, 255, 0.22) -2.94%, rgba(255, 255, 255, 0.22) 37.53%, rgba(255, 255, 255, 0) 50.8%, rgba(255, 255, 255, 0) 105%), #171717',
-      boxShadow: 'inset 2px 2px 2px rgba(255, 255, 255, 0.2)',
-      borderRadius: '0.5rem',
-      padding: '0.15rem'
-    },
-    texture: {
-      backgroundImage: `url(${ButtonTextureDark})`,
-      backgroundSize: '75%',
-      opacity: 0.05
-    }
-  }
-}
-
 interface SquareButtonState {
   active: boolean,
   hover: boolean
@@ -49,32 +33,21 @@ interface SquareButtonState {
 export function SquareButton ({
   color
 }: SquareButtonProps) {
-  const [state, setState] = useState<SquareButtonState>({
-    active: false,
-    hover: false
-  })
   return (
-    <div 
-      className='relative' 
-      data-active={state}
-      onMouseDown={() => setState(state => ({ ...state, active: true }))}
-      onMouseUp={() => setState(state => ({ ...state, active: false }))}
-      onMouseOver={() => setState(state => ({ ...state, hover: true }))}
-      onMouseOut={() => setState(state => ({ ...state, hover: false }))}>
-      <div className={state.hover === true ? 'opacity-0' : 'opacity-100'}>
+    <div className='relative group/button ease-linear'>
+      <div className='group-hover/button:opacity-90 opacity-100 transition-all'>
         <div className='absolute left-0 bottom-0 w-3 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,0.3)]'/>
         <div className='absolute right-0 bottom-0 w-1 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,0.3)]' />
         <div className='absolute right-0 top-0 w-[1px] h-6 shadow-[1px_0px_1px_rgba(255,255,255,0.3)]' />
       </div>
+      <div className='w-full h-full absolute shadow-[1px_1px_1px_rgba(255,255,255,0.8)] group-hover/button:opacity-10 transition-all opacity-0' />
       <div className='flex flex-col items-center justify-center p-[2px] rounded-[1px] bg-[#050404]'>
         <div className='relative'>
           <button className='z-10 relative w-24 h-24 flex flex-col items-center bg-[#171717] justify-center bg-gradient-to-br from-white/50 to-60% to-black/50 rounded-2 p-[0.1rem] shadow-[inset_2px_2px_2px_rgba(255,255,255,0.1)]'>
-            <div className='absolute -bottom-3 -right-2 w-24 h-24 bg-black blur-[0.5rem] z-0 rounded-2 opacity-80'></div>
+            <div className="absolute mt-[0.5px] ml-[0.5px] w-8 h-8 rounded-tl-2 left-0 top-0 from-white to-30% bg-gradient-to-br" />
+            <div className='absolute w-24 h-24 bg-black z-0 rounded-2 opacity-80 transition-all -bottom-3 -right-2 blur-[0.5rem]' />
             <div className='relative h-full w-full overflow-hidden rounded-[calc(0.5rem-1px)]'>
-              <div 
-                className='absolute z-10 h-full w-full'
-                style={buttonStyles?.[color]?.texture}>
-              </div>
+              <img src={DarkNoiseTexture} className='absolute z-10 h-full w-full opacity-5' />
               <div className='w-full h-full bg-[#1E1E1E]' />
             </div>
           </button>
