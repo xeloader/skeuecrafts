fbf6e22c7b03ff50e702744dbba96df36c609602
Victor Ingman <viregistermail@gmail.com>
2023-12-01 10:01:16 +0100

    :heavy_plus_sign: Add standard

--- src/components/ep133-ui/Button.tsx
+++ src/components/ep133-ui/Button.tsx
@@ -14,45 +14,45 @@ export enum Environment {
   Dark = 'dark'
 }
 
-export interface SquareButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  color: Colors,
-  symbol?: string
-}
-
 type ButtonStyles = {
   [key in Colors]?: {
-    base: React.CSSProperties,
+    base: React.CSSProperties
     texture: React.CSSProperties
   }
 }
 
-interface SquareButtonState {
-  active: boolean,
-  hover: boolean
+export interface SquareButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color: Colors
+  symbol?: string
+  lightIntensity?: number
 }
 
 export function SquareButton ({
   color,
   value,
   symbol,
+  lightIntensity = 0.9,
   onClick
-}: SquareButtonProps) {
-  const style = { "--light-intensity": 0.9 } as React.CSSProperties;
+}: SquareButtonProps): JSX.Element {
+  const style = { '--light-intensity': lightIntensity } as React.CSSProperties // eslint-disable-line
   return (
-    <div className='relative group/button ease-out font-ep133 [&_*]:duration-100 [&_*]:transition-all' style={style}>
+    <div
+      style={style}
+      className='relative group/button ease-out font-ep133 [&_*]:duration-100 [&_*]:transition-all'
+    >
       <div className='group-hover/button:opacity-90 opacity-100 transition-all group-active/button:opacity-0'>
-        <div className='absolute left-0 bottom-0 w-3 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,var(--light-intensity))]'/>
+        <div className='absolute left-0 bottom-0 w-3 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,var(--light-intensity))]' />
         <div className='absolute right-0 bottom-0 w-1 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,var(--light-intensity))]' />
         <div className='absolute right-0 top-0 w-[1px] h-6 shadow-[1px_0px_1px_rgba(255,255,255,var(--light-intensity))]' />
       </div>
       <div className='w-full h-full absolute shadow-[1px_1px_1px_rgba(255,255,255,0.8)] transition-all opacity-0 group-active/button:opacity-100' />
       <div className='flex flex-col items-center justify-center p-[2px] rounded-[1px] bg-[#050404]'>
         <div className='relative'>
-          <button 
+          <button
             onClick={onClick}
             className='z-10 relative w-24 h-24 flex flex-col items-center bg-[#171717] justify-center bg-gradient-to-br from-white/50 to-60% to-black/50 rounded-2 p-[0.1rem] shadow-[inset_2px_2px_2px_rgba(255,255,255,0.1)] group-active/button:from-white/10 transition-all'
           >
-            <div className="absolute mt-[0.5px] ml-[0.5px] w-8 h-8 rounded-tl-2 left-0 top-0 from-white/75 to-25% bg-gradient-to-br group-active/button:opacity-0" />
+            <div className='absolute mt-[0.5px] ml-[0.5px] w-8 h-8 rounded-tl-2 left-0 top-0 from-white/75 to-25% bg-gradient-to-br group-active/button:opacity-0' />
             <div className='absolute w-24 h-24 bg-black z-0 rounded-2 opacity-80 transition-all group-hover/button:-bottom-2.5 group-hover/button:-right-1.5 group-hover/button:blur-[0.4rem] -bottom-3 -right-2 blur-[0.5rem] group-active/button:opacity-0 group-active/button:blur-[0.1rem]' />
             <div className='relative h-full w-full overflow-hidden rounded-[calc(0.5rem-1px)]'>
               <div className='absolute z-20 h-full w-full group-hover/button:-translate-y-[0.5px] group-hover/button:-translate-x-[0.5px] group-active/button:-translate-y-[1.5px] group-active/button:-translate-x-[1.5px]'>
