35d6f5548feb300187a342e77379f4bcd856f33e
Victor Ingman <viregistermail@gmail.com>
2023-12-11 16:32:05 +0100

    :sparkles: Last row on display implemented

--- src/components/ep133-ui/DisplayMatrix.tsx
+++ src/components/ep133-ui/DisplayMatrix.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SegmentDisplay from './SegmentDisplay'
+import * as Symbol from './Symbols'
 
 export interface DisplayMatrixProps {
   value?: string
@@ -14,8 +15,26 @@ export default function DisplayMatrix ({
     <div
       className='grid grid-cols-20 grid-rows-4 gap-2'
     >
-      <SegmentDisplay value={value} dotValue={dotValue} className='h-16 col-start-8 col-end-[12] row-start-1 row-end-3 pr-2 -ml-2' />
-      {Array.from(Array((20 * 4) - (4 * 2))).map((_, i) => <div key={i} className='bg-[red] w-6 h-6'>{i}</div>)}
+      <Symbol.Battery className='col-start-1 col-span-1 row-start-1 row-span-1' />
+      <SegmentDisplay value={value} dotValue={dotValue} className='h-16 col-start-8 col-span-4 row-start-1 row-span-2 pr-2 -ml-2' />
+      {/* {Array.from(Array((20 * 4) - (4 * 2))).map((_, i) => <div key={i} className='bg-[red] w-6 h-6'>{i}</div>)} */}
+      <Symbol.BPMLabel className='col-start-12 col-span-1' />
+      <Symbol.RecordPlayer className='col-start-[15] col-span-1 row-start-2 row-span-1' />
+      {/* Last Row */}
+      <Symbol.Slider className='col-start-1 row-start-4 col-span-1 row-span-1' />
+      <Symbol.LowestFade className='col-start-2 row-start-4 col-span-1 row-span-1' />
+      <Symbol.DButton className='col-start-3 row-start-4 col-span-1 row-span-1' />
+      <Symbol.SoundSign className='col-start-4 row-start-4 col-span-2 row-span-1' />
+      <Symbol.BrainPerson className='col-start-6 row-start-4 col-span-1 row-span-1' />
+      <Symbol.MainSign className='col-start-7 row-start-4 col-span-2 row-span-1' />
+      <Symbol.SpinWheel className='col-start-9 row-start-4 col-span-1 row-span-1' />
+      <Symbol.TempoSign className='col-start-[10] col-span-2 row-start-4 row-span-1' />
+      <Symbol.EraseSign className='col-start-[12] col-span-2 row-start-4 row-span-1' />
+      <Symbol.SystemSign className='col-start-[14] col-span-2 row-start-4 row-span-1' />
+      <Symbol.Laptop className='col-start-[16] row-start-4 col-span-1 row-span-1' />
+      <Symbol.ClipWarning className='col-start-[17] row-start-4 col-span-1 row-span-1' />
+      <Symbol.LowLevel className='col-start-[18] row-start-4 col-span-1 row-span-1' />
+      <Symbol.Boxer className='col-start-[19] col-span-2 row-start-4 row-span-1' />
     </div>
   )
 }
