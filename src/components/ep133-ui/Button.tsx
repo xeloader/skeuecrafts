06e8af2580070631a24f2cf4032460280b01fc4f
Victor Ingman <viregistermail@gmail.com>
2023-12-11 10:18:55 +0100

    :lipstick: Fix lightintensity for full reflection

--- src/components/ep133-ui/Button.tsx
+++ src/components/ep133-ui/Button.tsx
@@ -101,7 +101,7 @@ export const Hole = ({
         <div className='absolute right-0 bottom-0 w-1 h-[1px] shadow-[0px_1px_1px_rgba(255,255,255,var(--light-intensity))]' />
         <div className='absolute right-0 top-0 w-[1px] h-6 shadow-[1px_0px_1px_rgba(255,255,255,var(--light-intensity))]' />
       </div>
-      <div className='w-full h-full absolute shadow-[1px_1px_1px_rgba(255,255,255,0.8)] transition-all opacity-0 group-active/button:opacity-100' />
+      <div className='w-full h-full absolute shadow-[1px_1px_1px_rgba(255,255,255,calc(0.8*var(--light-intensity)))] transition-all opacity-0 group-active/button:opacity-100' />
       <div className='flex flex-col items-center justify-center p-[2px] rounded-[1px] bg-[#050404]'>
         {children}
       </div>
