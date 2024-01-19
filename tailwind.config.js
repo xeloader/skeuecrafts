a65a27df94e20259bb75da17e579fa1d2dab9873
Victor Ingman <viregistermail@gmail.com>
2024-01-19 13:19:06 +0100

    :construction: Stash force pseudo state for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
-const plugin = require('tailwindcss/plugin')
+import { INTERNAL_FEATURES } from 'tailwindcss/src/lib/setupContextUtils'
+import { normalize } from 'tailwindcss/src/util/dataTypes'
+import escapeClassName from 'tailwindcss/src/util/escapeClassName'
+import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -62,3 +65,76 @@ module.exports = {
     })
   ]
 }
+
+const WIPForcePseudo = plugin(function ({ addVariant, matchVariant, prefix }) {
+  const pseudoVariants = [
+    ['active', '&.is-activated', '&:active'],
+    ['hover', '&.is-hovered']
+  ].map((variant) => (Array.isArray(variant) ? variant : [variant, `&:${variant}`]))
+
+  for (const [variantName, ...states] of pseudoVariants) {
+    for (const state of states) {
+      addVariant(variantName, (ctx) => {
+        const result = typeof state === 'function' ? state(ctx) : state
+
+        return result
+      })
+    }
+  }
+
+  const variants = {
+    group: (_, { modifier }) =>
+      modifier
+        ? [`:merge(${prefix('.group')}\\/${escapeClassName(modifier)})`, ' &']
+        : [`:merge(${prefix('.group')})`, ' &'],
+    peer: (_, { modifier }) =>
+      modifier
+        ? [`:merge(${prefix('.peer')}\\/${escapeClassName(modifier)})`, ' ~ &']
+        : [`:merge(${prefix('.peer')})`, ' ~ &']
+  }
+
+  for (const [name, fn] of Object.entries(variants)) {
+    matchVariant(
+      name,
+      (value = '', extra) => {
+        let result = normalize(typeof value === 'function' ? value(extra) : value)
+        console.log('result', result, 'modifier', extra?.modifier, 'name', name)
+        if (!result.includes('&')) result = '&' + result
+
+        const [a, b] = fn('', extra)
+
+        let start = null
+        let end = null
+        let quotes = 0
+
+        for (let i = 0; i < result.length; ++i) {
+          const c = result[i]
+          if (c === '&') {
+            start = i
+          } else if (c === "'" || c === '"') {
+            quotes += 1
+          } else if (start !== null && c === ' ' && !quotes) {
+            end = i
+          }
+        }
+
+        if (start !== null && end === null) {
+          end = result.length
+        }
+
+        // Basically this but can handle quotes:
+        // result.replace(/&(\S+)?/g, (_, pseudo = '') => a + pseudo + b)
+        const cut = result.slice(0, start) + a + result.slice(start + 1, end) + b + result.slice(end)
+        console.log('return', cut)
+        console.log()
+        return cut
+      },
+      {
+        values: Object.fromEntries(pseudoVariants),
+        [INTERNAL_FEATURES]: {
+          respectPrefix: false
+        }
+      }
+    )
+  }
+})
