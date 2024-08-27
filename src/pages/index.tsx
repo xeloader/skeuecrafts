9c82e3be3e7775883406df76b808ab4bbf0455d3
Victor Ingman <viregistermail@gmail.com>
2024-08-27 22:43:50 +0200

    Add title animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function IndexPage (): JSX.Element {
   return (
     <main>
       <div className='flex flex-col gap-1 px-1 text-xs bg-[blue] text-white'>
-        <h1 className='text-md'>paint w. code</h1>
+        <h1 className='text-md'>skojcrafts / paint w. code</h1>
         <p className='text-black/50' />
       </div>
       <ul className='text-lg pl-1'>
@@ -15,4 +15,26 @@ export default function IndexPage (): JSX.Element {
   )
 }
 
-export const Head: HeadFC = () => <title>Home Page</title>
+const getFrame = (frames: string[], index: number): string => {
+  return frames[index % frames.length]
+}
+
+export const Head: HeadFC = () => {
+  const [index, setIndex] = React.useState(0)
+  const faces = ['·', '.', '.', '·', '˙', '˙']
+  const frames = faces.length * 2
+  React.useEffect(() => {
+    const id = setInterval(() => {
+      if (!document.hidden) {
+        setIndex(index => index + 1)
+      }
+    }, 250)
+    return () => {
+      clearInterval(id)
+    }
+  })
+  const completeFrame = Array.from(Array(frames)).map((_, i) => getFrame(faces, index + i + 1)).join('')
+  return (
+    <title>.skojcrafts {completeFrame}</title>
+  )
+}
